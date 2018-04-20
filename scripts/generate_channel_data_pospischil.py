662320a72e3f98222ef98ac953dc657fee41f217
janfb <j.f.boelts@gmail.com>
2018-04-20 11:47:26 +0200

    update script

--- scripts/generate_channel_data_pospischil.py
+++ scripts/generate_channel_data_pospischil.py
@@ -1,22 +1,15 @@
 import delfi.distribution as dd
-import matplotlib as mpl
 import numpy as np
 import os
-import pandas as pd
 import pickle
 import time
 
-from delfi.generator import Default, MPGenerator
-from delfi.utils.viz import plot_pdf
-
 from lfimodels.channelomics.ChannelSingle import ChannelSingle
-from lfimodels.channelomics.ChannelSuper import ChannelSuper
 from lfimodels.channelomics.ChannelStats import ChannelStats
 from lfimodels.channelomics.ChannelMPGenerator import ChannelMPGenerator
-from matplotlib import pyplot as plt
 
 
-## GOAL of this script: generate k and na channel data and save to disk for later training
+# GOAL of this script: generate k and na channel data and save to disk for later training
 n_samples = 100
 seed = 1
 cython = True
@@ -36,8 +29,6 @@ prior_lims_k = np.sort(np.concatenate((0.3 * gt_k.reshape(-1,1), 1.3 * gt_k.resh
 n_workers = 4
 
 # as we use k as gt, the model is already set up..
-# mk = ChannelSingle(channel_type='k', n_params=len(gt_k), cython=cython, seed=seed)
-
 model_seeds = np.arange(1, n_workers + 1)
 mks = [ChannelSingle(channel_type='kd', n_params=len(gt_k), cython=cython, seed=model_seeds[i]) for i in range(n_workers)]
 pk = dd.Uniform(lower=prior_lims_k[:, 0], upper=prior_lims_k[:, 1], seed=seed)
@@ -45,11 +36,10 @@ sk = ChannelStats(channel_type='kd', seed=seed)
 # gk = Default(model=mk, summary=sk, prior=pk, seed=seed)
 gk = ChannelMPGenerator(models=mks, summary=sk, prior=pk, seed=seed)
 
-# set up na model
+# set up kslow model
 gt_ks = GT['kslow']
 prior_lims_ks = np.sort(np.concatenate((0.3 * gt_ks.reshape(-1, 1), 1.3 * gt_ks.reshape(-1, 1)), axis=1))
 
-# mna = ChannelSingle(channel_type='na', n_params=len(gt_na), cython=cython, seed=seed)
 model_seeds = np.arange(n_workers, 2 * n_workers)
 mnas = [ChannelSingle(channel_type='kslow', n_params=len(gt_ks), cython=cython, seed=model_seeds[i]) for i in range(n_workers)]
 pna = dd.Uniform(lower=prior_lims_ks[:, 0], upper=prior_lims_ks[:, 1], seed=seed)
